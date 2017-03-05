25f4d4d12476eece0eb70ce9d5f9153ba8da14c0
Ovidiu Grigoras <oviduzz@yahoo.com>
2017-03-05 22:21:52 +0200

    added color block function

--- src/js/app.js
+++ src/js/app.js
@@ -5,16 +5,20 @@ var addButton = document.getElementById('add-car'),
     inputTitle =  document.getElementById('input-title'),
     inputYear =  document.getElementById('input-year'),
     inputMileage =  document.getElementById('input-mileage'),
+    inputPrice =  document.getElementById('input-price'),
     inputBrand =  document.getElementById('input-brand'),
     error = document.getElementById('error-name'),
     errorNumber = document.getElementById('error-number'),
     errorMileage = document.getElementById('error-mileage'),
+    errorPrice = document.getElementById('error-price'),
     numberReg = new RegExp('^\\d+$'),
 
     getImageId = document.getElementById("logo-brand"),
     getDamageBlock = document.getElementById("input-damage"),
     getDamage = document.getElementById("radioTrue"),
-    damageField = document.getElementById("crash-details");
+    getColor = document.getElementById("input-color"),
+    damageField = document.getElementById("crash-details"),
+    colorBox = document.getElementById('color-block');
 
 
 addButton.onclick = function(){
@@ -35,7 +39,7 @@ getDamageBlock.onclick = function (){
 };
 
 // set image src by what option value is chosen
-inputBrand.onclick = function (){
+inputBrand.onchange = function (){
     switch(inputBrand.options[inputBrand.selectedIndex].value){
         case 'hyundai':
             getImageId.src = '../img/hyundai-logo.png';
@@ -55,11 +59,24 @@ inputBrand.onclick = function (){
         default:
             break;
     }
+};
+
+// changing color 'real-time' of square
+getColor.onkeydown = function () {
+    setTimeout(function(){
+        colorBox.style.display = 'block';
+        colorBox.style.backgroundColor = getColor.value;
+
+        //if input color is empty we hide it
+        if (getColor.value.length == 0) {
+            colorBox.style.display = 'none';
+        }
+    },0);
 
 };
 
 saveButton.onclick = function(){
-    //checking if title has more than 3 characters which is making title valid
+    //checking if title has characters to make it valid
     if (inputTitle.value.length < 1) {
         error.style.display = 'block';
     } else {
@@ -67,15 +84,21 @@ saveButton.onclick = function(){
     }
 
     //checking if filed is number
-    if (numberReg.test(inputYear.value) === false) {
+    if (numberReg.test(inputYear.value) === false && inputYear.value.length > 0) {
         errorNumber.innerHTML = 'This field must be a number';
     } else {
         errorNumber.innerHTML = '';
     }
-    if (numberReg.test(inputMileage.value) === false){
+    if (numberReg.test(inputMileage.value) === false && inputMileage.value.length > 0){
         errorMileage.innerHTML = 'This field must be a number';
     } else {
         errorMileage.innerHTML = '';
+    }
+
+    if (numberReg.test(inputPrice.value) === false && inputPrice.value.length > 0){
+        errorPrice.innerHTML = 'This field must be a number';
+    } else {
+        errorPrice.innerHTML = '';
     }
 
 };

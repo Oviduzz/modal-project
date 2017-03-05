97a0cca2a279c819830902c7b9510842a91c1d07
Ovidiu Grigoras <oviduzz@yahoo.com>
2017-03-05 21:21:30 +0200

    added switch case for logos // added checking for numbers when submit

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,17 @@
 var addButton = document.getElementById('add-car'),
     saveButton = document.getElementById('save-car'),
     cancelButton = document.getElementById('cancel-car'),
-    modal = document.getElementById('modal-block');
+    modal = document.getElementById('modal-block'),
+    inputTitle =  document.getElementById('input-title'),
+    inputYear =  document.getElementById('input-year'),
+    inputMileage =  document.getElementById('input-mileage'),
+    inputBrand =  document.getElementById('input-brand'),
+    error = document.getElementById('error-name'),
+    errorNumber = document.getElementById('error-number'),
+    errorMileage = document.getElementById('error-mileage'),
+    numberReg = new RegExp('^\\d+$'),
+
+    getImageId = document.getElementById("logo-brand");
 
 
 addButton.onclick = function(){
@@ -12,6 +22,48 @@ cancelButton.onclick = function(){
     modal.style.display = 'none';
 };
 
+// set image src by what option value is chosen
+inputBrand.onclick = function (){
+    switch(inputBrand.options[inputBrand.selectedIndex].value){
+        case 'hyundai':
+            getImageId.src = '../img/hyundai-logo.png';
+            break;
+        case 'ford':
+            getImageId.src = '../img/ford-logo.png';
+            break;
+        case 'bmw':
+            getImageId.src = '../img/bmw-logo.png';
+            break;
+        case 'mercedes':
+            getImageId.src = '../img/mercedes-logo.png';
+            break;
+        case 'choose':
+            getImageId.src = '';
+            break;
+        default:
+            break;
+    }
+
+};
+
 saveButton.onclick = function(){
-    modal.style.display = 'none';
+    //checking if title has more than 3 characters which is making title valid
+    if (inputTitle.value.length < 1) {
+        error.style.display = 'block';
+    } else {
+        error.style.display = 'none';
+    }
+
+    //checking if filed is number
+    if (numberReg.test(inputYear.value) === false) {
+        errorNumber.innerHTML = 'This field must be a number';
+    } else {
+        errorNumber.innerHTML = '';
+    }
+    if (numberReg.test(inputMileage.value) === false){
+        errorMileage.innerHTML = 'This field must be a number';
+    } else {
+        errorMileage.innerHTML = '';
+    }
+
 };
